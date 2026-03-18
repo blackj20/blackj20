d8f5efaf52a707b6c37fc2f44783e1d2f1bbf3ef
blackj20 <jswebolo3@gmail.com>
2026-03-18 16:59:38 +0100

    amelioration du js accueil,ajout dan apropos et actualite retout tage globale

--- public/js/accueil.js
+++ public/js/accueil.js
@@ -1,6 +1,4 @@
-
-
-const api_actualite=[
+const api_actualite=[// ------------------------- similation des donnés-------------------
     { titre:"maison de 5 etage a ngombe" ,img:"image/WhatsApp Image 2026-03-16 at 10.05.27(2).jpeg",data_description:"appartement meuble si tue a ..." },
     { titre:"hotel trois etoile" ,img:"image/WhatsApp Image 2026-03-16 at 10.05.26(1).jpeg",data_description:"des suite de luxe cliumatise avec ..." },
     { titre:" appartement de trois niveaux" ,img:"image/WhatsApp Image 2026-03-16 at 09.44.52(1).jpeg",data_description:"appartement de haut quanlites dans des ..." },
@@ -16,83 +14,60 @@ const api_realisation=[
 const api_project=[
 
     { titre:"roject_numero_1" ,img:"image/WhatsApp Image 2026-03-16 at 09.44.51(1).jpeg",data_description:"..." },
-    { titre:"roject_numero_2" ,img:"image/WhatsApp Image 2026-03-16 at 09.44.45(1).jpeg",data_description:"..." },
+    { titre:"roject_numero_2" ,img:'image/WhatsApp Image 2026-03-16 at 09.44.36.jpeg',data_description:"..." },
     { titre:"roject_numero_3" ,img:"image/WhatsApp Image 2026-03-16 at 09.44.41.jpeg",data_description:"..." }
 ]
 
 
-const realisation=(data_api)=>{
+
+const poste=(data_api,parent="",ClassName="item")=>{
+
+    if(!parent)return alert("erreur arrét 'affichage  parent manquant !")
 
     const { titre ,img,data_description,anneé,localisation }=data_api // on retire tout les donne du data 
 
-    const data =document.createElement("div") // le contenneur
-    const  titre_=document.createElement("h3")   // titre de la realisation
-    const  image =document.createElement("img") // image de la realisation 
-    const  anneé_=document.createElement("p")
-    const  localisation_=document.createElement("p")
-    const  descrp= document.createElement("p")// description sur la realisation
-    
-    data.className="item"
-
-    titre_.textContent=titre
-    image.src=img
-    descrp.textContent=data_description
-    anneé_.textContent=anneé
-    localisation_.textContent=localisation
-    data.append(titre_,image,descrp,anneé_,localisation_)
-    document.getElementById("realisation-container").appendChild(data)
-
-}
-
-const actualité=(data_api)=>{
-
-    const { titre ,img,data_description }=data_api // on retire tout les donne du data 
-
-    const data =document.createElement("div") // le contenneur
+    const cart =document.createElement("div") // le contenneur
     const  titre_=document.createElement("h3")   // titre de la realisation
     const  image =document.createElement("img") // image de la realisation  
     const  descrp= document.createElement("p")// description sur la realisation
     
-    data.className="item"
-
+    cart.className=ClassName
     titre_.textContent=titre
-    image.src=img
+    image.src=img||"image/logo.png"
     descrp.textContent=data_description
-    data.append(titre_,image,descrp)
-    document.getElementById("actualite-container").appendChild(data)
-}
-const projects =(data_api)=>{
-
-    const { titre ,img,data_description }=data_api // on retire tout les donne du data 
-
-    const data =document.createElement("div") // le contenneur
-    const  titre_=document.createElement("h3")   // titre de la realisation
-    const  image =document.createElement("img") // image de la realisation  
-    const  descrp= document.createElement("p")// description sur la realisation
+    cart.append(titre_,image,descrp)
     
-    data.className="cart-item"
+    if(anneé){
+        const year=document.createElement("p")
+        year.textContent=` anné : ${anneé}`
+        cart.append(year)
+    }
+    if(localisation){
+        const loc=document.createElement("p")
+        loc.textContent=`localisation : ${localisation}`
+        cart.append(loc)
+    }
+     
 
-    titre_.textContent=titre
-    image.src=img
-    descrp.textContent=data_description
-    data.append(titre_,image,descrp)
-    document.getElementById("projects").appendChild(data)
+    document.getElementById(parent).append(cart)
 }
+
 
 const loadindingImg=(data_actu,data_projt,data_realis)=>{// on cree des poste en boucle
 
     data_projt.forEach(project => {
-        projects(project) 
-        console.log("projrct OK")
+        poste(project,"projects") 
     });
+
     data_actu.forEach(actu => {
-        actualité(actu) 
-        console.log("actualité OK")
+        poste(actu,"actualite-container") 
+    
     });
     data_realis.forEach(realis => {
-        realisation(realis) 
-        console.log("realisation  OK")
+       poste(realis,"realisation-container") 
+       
     });
 }
 
 loadindingImg(api_actualite,api_project,api_realisation)
+
