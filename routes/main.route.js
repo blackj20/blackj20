80f994b06bb2876106199e15884f172d21e2d6b7
blackj20 <jswebolo3@gmail.com>
2026-04-10 14:01:37 +0100

    EMC v 1.5.0 correction des route facon backend

--- routes/main.route.js
+++ routes/main.route.js
@@ -16,28 +16,33 @@ router.get('/', (req,res)=>{
    res.sendFile(path.join(__dirname,'../public/accueil.html'))
 } )
 
+router.get('/accueil', (req,res)=>{
+   res.sendFile(path.join(__dirname,'../public/accueil.html'))
+} )
+
+router.get('/apropos', (req,res)=>{
+   res.sendFile(path.join(__dirname,'../public/about.html'))
+} )
+
+router.get('/service', (req,res)=>{
+   res.sendFile(path.join(__dirname,'../public/service.html'))
+} )
+
+router.get('/realisation', (req,res)=>{
+   res.sendFile(path.join(__dirname,'../public/realisation.html'))
+} )
+
+router.get('/actualites', (req,res)=>{
+   res.sendFile(path.join(__dirname,'../public/actualiter.html'))
+} )
+
+router.get('/annonce', (req,res)=>{
+   res.sendFile(path.join(__dirname,'../public/annonce.html'))
+} )
+
 router.get('/error', (req, res) => {
    res.sendFile(path.join(__dirname,'../private/error.html'))
 })
-// router.get('/apropos', (req,res)=>{
-//    res.sendFile(path.join(__dirname,'../public/about.html'))
-// } )
-// router.get('/service', (req,res)=>{
-//    res.sendFile(path.join(__dirname,'../public/service.html'))
-// } )
-// router.get('/realisation', (req,res)=>{
-//    res.sendFile(path.join(__dirname,'../public/realisation.html'))
-// } )
-// router.get('/actualites', (req,res)=>{
-//    res.sendFile(path.join(__dirname,'../public/actualiter.html'))
-
-// } )
-// router.get('/annonce', (req,res)=>{
-//    res.sendFile(path.join(__dirname,'../public/annonce.html'))
-// } )
-// router.get('/login', (req,res)=>{
-//    res.sendFile(path.join(__dirname,'../public/login.html'))
-// } )
 // ==========================post routes ========================
 
 
