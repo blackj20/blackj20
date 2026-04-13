7908c739eb7557a59c16c8475bf7c2218cb0d352
blackj20 <jswebolo3@gmail.com>
2026-04-13 08:57:22 +0100

    EMC v 2.2.0

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const express = require('express');
 const app =require('./app')
 
 
-const {PORT, url}= process.env
+const {PORT,local_url}= process.env
 
 app.listen(PORT, '0.0.0.0',() => {
-    console.log(`serveur lanser au port ${PORT} cliquez le lien pour rejoindre ${url}:${PORT} ctrl+c pour stoper le serveur`);
+    console.log(`serveur lanser au port ${PORT} cliquez le lien pour rejoindre ${local_url}:${PORT} ctrl+c pour stoper le serveur`);
 }); 
 
 // done!!
